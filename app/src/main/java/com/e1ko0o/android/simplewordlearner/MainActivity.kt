d266591de1ed162539d253af4e6e42b1861b2b13
E1ko0o <slava121231@mail.ru>
2022-02-02 20:22:45 +0300

    Initital commit

---
+++ app/src/main/java/com/e1ko0o/android/simplewordlearner/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.e1ko0o.android.simplewordlearner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
